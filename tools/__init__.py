146a62c3ba85a45ad0fb67a49e73a1502eb73a56
D1245806 <k0917365633@gmail.com>
2026-04-01 20:16:55 +0800

    feat: add search_travel_tips tool using duckduckgo-search API

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,5 +1,6 @@
 from .weather import get_city_weather
 from .pdf_reader import read_pdf_content
 from .code_reader import read_code_files
+from .searching_tips import search_travel_tips
 
-__all__ = ["get_city_weather", "read_pdf_content", "read_code_files"]
+__all__ = ["get_city_weather", "read_pdf_content", "read_code_files", "search_travel_tips"]
