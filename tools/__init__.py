0244608460f9b8c4cae719bbf37e4460cd3c42e1
Dennis <dennis@example.com>
2026-04-01 11:59:52 +0800

    1

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,5 +1,6 @@
 from .weather import get_city_weather
 from .pdf_reader import read_pdf_content
 from .code_reader import read_code_files
+from .advice import get_travel_advice
 
-__all__ = ["get_city_weather", "read_pdf_content", "read_code_files"]
+__all__ = ["get_city_weather", "read_pdf_content", "read_code_files", "get_travel_advice"]
